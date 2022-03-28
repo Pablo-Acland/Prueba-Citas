8155c20589f1afdfb71d8b6d4b07b19aedbbb3d0
Pablo Acland <pablo.acland@estudiantes.utec.edu.uy>
2022-03-28 17:46:06 -0300

    Entrega

---
+++ src/main/java/com/springBajo8/springBajo8/domain/PadecimientoTratamiento.java
@@ -0,0 +1,36 @@
+package com.springBajo8.springBajo8.domain;
+
+public class PadecimientoTratamiento {
+    private  String padecimiento;
+    private  String tratamiento;
+
+    public PadecimientoTratamiento(String padecimiento, String tratamiento) {
+        this.padecimiento = padecimiento;
+        this.tratamiento = tratamiento;
+    }
+
+    public PadecimientoTratamiento() {
+    }
+
+    public String getPadecimiento() {
+        return padecimiento;
+    }
+
+    public void setPadecimiento(String padecimiento) {
+        this.padecimiento = padecimiento;
+    }
+
+    public String getTratamiento() {
+        return tratamiento;
+    }
+
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+
+    @Override
+    public String toString() {
+        return "padecimiento = '" + padecimiento + '\'' +
+                ", Tratamiento = '" + tratamiento + '\'';
+    }
+}
